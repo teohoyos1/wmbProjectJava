2bd5b2d1a23ed4f177f20ec5f6f5e351a47f5127
teohoyos1 <mateorojashoyos@gmail.com>
2023-01-16 02:02:32 -0500

    function added http get path to Bank

--- src/main/java/com/wmb/project/web/controller/BankController.java
+++ src/main/java/com/wmb/project/web/controller/BankController.java
@@ -4,10 +4,12 @@ import com.wmb.project.domain.BankD;
 import com.wmb.project.domain.service.BankService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/bank")
@@ -18,5 +20,10 @@ public class BankController {
     @GetMapping("/all")
     public List<BankD> getAll() {
         return bankService.getAllBanks();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<BankD> getBank(@PathVariable("id") Integer bankId){
+        return this.bankService.getBank(bankId);
     }
 }

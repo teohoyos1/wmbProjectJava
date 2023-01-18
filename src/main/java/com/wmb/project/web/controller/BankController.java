268e259c22370757b3fa9c9ffd6e690eef2fba3d
teohoyos1 <mateorojashoyos@gmail.com>
2023-01-17 21:22:29 -0500

    add swagger dependency, documentation UI and annotation in class

--- src/main/java/com/wmb/project/web/controller/BankController.java
+++ src/main/java/com/wmb/project/web/controller/BankController.java
@@ -2,6 +2,10 @@ package com.wmb.project.web.controller;
 
 import com.wmb.project.domain.BankD;
 import com.wmb.project.domain.service.BankService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,24 +24,40 @@ public class BankController {
     BankService bankService;
 
     @GetMapping("/all")
+    @ApiOperation("Get all banks")
+    @ApiResponse(code=200, message = "OK")
     public ResponseEntity<List<BankD>> getAll() {
         return new ResponseEntity<>(bankService.getAllBanks(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<BankD> getBank(@PathVariable("id") Integer bankId){
+    @ApiOperation("Get bank by ID")
+    @ApiResponses({
+            @ApiResponse(code=200, message = "OK"),
+            @ApiResponse(code=404, message = "Bank not found"),
+    })
+    public ResponseEntity<BankD> getBank(@ApiParam(value = "The id of the bank", required = true, example = "7")
+                                             @PathVariable("id") Integer bankId){
         return this.bankService.getBank(bankId)
                 .map(bankD -> new ResponseEntity<>(bankD, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping("/save")
+    @ApiOperation("Save a new bank")
+    @ApiResponse(code = 200, message = "OK")
     public BankD save(@RequestBody BankD bankD){
         return bankService.save(bankD);
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<List<BankD>> delete(@PathVariable("id") int bankId){
+    @ApiOperation("Delete bank by ID")
+    @ApiResponses({
+            @ApiResponse(code=200, message = "OK"),
+            @ApiResponse(code=404, message = "Bank not found"),
+    })
+    public ResponseEntity<List<BankD>> delete(@ApiParam(value = "The id of the bank", required = true, example = "7")
+                                                  @PathVariable("id") int bankId){
         if (!bankService.delete(bankId)){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
@@ -45,6 +65,11 @@ public class BankController {
     }
 
     @PutMapping("/update")
+    @ApiOperation("Update bank")
+    @ApiResponses({
+            @ApiResponse(code=200, message = "OK"),
+            @ApiResponse(code=404, message = "Bank not found"),
+    })
     public ResponseEntity<BankD> update(@RequestBody BankD bankD){
         //No viene ningún id en el update
         if (bankD == null || bankD.getBankId() == 0){
